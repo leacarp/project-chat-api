781cc0e3faeb567781f41701c50d2f206b427a2d
Leandro Aguirre <leanaguirre2016@gmail.com>
2025-08-21 23:28:57 -0300

    first commit

---
+++ src/views/IndecisionView.vue
@@ -0,0 +1,15 @@
+<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Dad</span>
+    </div>
+    <ChatMessage />
+    <MessageBox />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatMessage from '@/components/chat/ChatMessage.vue';
+import MessageBox from '@/components/chat/MessageBox.vue';
+</script>
